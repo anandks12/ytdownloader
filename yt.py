cd68408f114c4e04cc3dfd3d429498024a8582bd
appuks001 <anandsudesan001@gmail.com>
2023-05-13 19:38:58 +0530

    Hello

--- yt.py
+++ yt.py
@@ -4,7 +4,7 @@ video_list = open("sample.txt","r")
 for i in video_list:
     yt = YouTube(i)
     try :
-        dw=yt.streams.first()
+        dw=yt.streams.get_highest_resolution()
         dw.download()
     except:
         print("Download failed for",i)
